1c2c422ff6adab688e3a0ffb89c0a4e8f78ee441
nicolasroger17 <nicolasroger17@gmail.com>
2014-08-19 21:06:08 +0200

    bullets destruction ok

--- webroot/js/game/game.js
+++ webroot/js/game/game.js
@@ -4,7 +4,10 @@ var TOWERS_DATA;
 var CASE_SIZE = 60;
 var BOARD_SIZE = {};
 
-var nbTowers = 0;
+var towers = {};
+var monsters = {};
+var bullets = {};
+
 var currentWave = 0;
 
 $(document).ready(function(){
@@ -61,81 +64,81 @@ function optimizePath(path){
 }
 
 function startWave(){
-	var numero = 0;
+	updateImpact();
 	var wave = setInterval(function(){
-		var type = MAP_DATA.monsters[currentWave][numero];
+		var type = MAP_DATA.monsters[currentWave][Object.size(monsters)];
 		var top = MAP_DATA.path[0].x + ((CASE_SIZE - MONSTERS_DATA[type].height) / 2);
 		var left = MAP_DATA.path[0].y + ((CASE_SIZE - MONSTERS_DATA[type].width) / 2);
-		$("#gameboard").append("<div id='monster" + numero + "' class='monster " + type + 
+		$("#gameboard").append("<div id='monster" + Object.size(monsters) + "' class='monster' type='" + type + 
 			"' style='top: " + top + "px; left: " + left + "px;'></div>");
-		moveMonster(numero);
-		numero++;
+		moveMonster(type);
 
-		if(numero == MAP_DATA.monsters[currentWave].length){
+		if(Object.size(monsters) == MAP_DATA.monsters[currentWave].length){
 			currentWave++;
 			clearInterval(wave);
 		}
-	}, 300);
+	}, 1000);
 }
 
-function moveMonster(numero){
+function moveMonster(type){
 	var pos = 1;
-	var move = setInterval(function(){
-		var newTop = parseInt($("#monster" + numero).css("top")) + MAP_DATA.path[pos].x;
-		var newLeft = parseInt($("#monster" + numero).css("left")) + MAP_DATA.path[pos].y;
-		$("#monster" + numero).css("top", newTop);
-		$("#monster" + numero).css("left", newLeft);
+	var e = $("#monster" + Object.size(monsters));
+	var move = setInterval(function(){		
+		var newTop = parseInt(e.css("top")) + MAP_DATA.path[pos].x;
+		var newLeft = parseInt(e.css("left")) + MAP_DATA.path[pos].y;
+		e.css("top", newTop);
+		e.css("left", newLeft);
 		pos++;
 
 		if(pos == MAP_DATA.path.length){
 			setTimeout(function(){
-				$("#monster" + numero).remove();
+				e.remove();
 			}, 600);
 			clearInterval(move);
 		}
 	}, 600);
+	monsters[e[0].id] = {element: e, health: MONSTERS_DATA[type].health, interval: move};
 }
 
 var towerInter = Array();
 function placeTower(element){
 	var pos = element.position();
-	var tower = "<div class='tower' id='tower" + nbTowers + "' style='top: " + pos.top + "px; left: " + pos.left + "px;'></div>"
+	var tower = "<div class='tower' id='tower" + Object.size(towers) + "' style='top: " + pos.top + "px; left: " + pos.left + "px;'></div>";
 	$("#gameboard").append(tower);
-	var t = $("#tower" + nbTowers);
+	var t = $("#tower" + Object.size(towers));
+
 	var inter = setInterval(function(){
 		if($(".monster").length > 0){
 			t.pointat({target: "#" + t.nearest($(".monster"))[0].id});
 			fire(t);
 		}
 	}, 200);
-	nbTowers++;
+	towers[t[0].id] = {element: t, interval: inter};
 }
 
-var nbBullets = 0;
 function fire(element){
 	var trajectory = determineEquation(element, getRotation(element));
 	var pos = element.offset();
 
-	$("#gameboard").append("<div id='bullet" + nbBullets + "' class='bullet' style='top: " +
+	$("#gameboard").append("<div id='bullet" + Object.size(bullets) + "' class='bullet' style='top: " +
 		trajectory.origin.y + "px; left: " + trajectory.origin.x + "px;'></div>");
-	var e = $("#bullet" + nbBullets);
+	var e = $("#bullet" + Object.size(bullets));
 
-	var pointsIncr = 0;
+	var pointsIncr = 1;
 	var bInter = setInterval(function(){
-		destroyBulletIfOut(e, bInter);
+		checkIfBulletIsOut(e[0].id, bInter);
 		e.css("top", trajectory.getY(pointsIncr));
 		e.css("left", trajectory.getX(pointsIncr));
 		pointsIncr++;
 	}, 200);
-	nbBullets++;
+	bullets[e[0].id] = {element: e, interval: bInter, damage: 10};
 }
 
-function destroyBulletIfOut(bullet, inter){
-	var pos = bullet.position();
+function checkIfBulletIsOut(id){
+	var pos = bullets[id].element.position();
 	if(pos.top <= BOARD_SIZE.y1 || pos.top > BOARD_SIZE.y2
 		|| pos.left < BOARD_SIZE.x1 || pos.left > BOARD_SIZE.x2){
-		bullet[0].remove();
-		clearInterval(inter);
+		destroyBullet(bullets[id]);
 	}
 }
 
@@ -180,12 +183,87 @@ function determineEquation(element, angle){
 	}
 
 	return {
-			origin : {x: (x1 + x2), y: (y1 + y2)},
-			getX: function(incr){
-				return (x1 + x2) + (incr * x2);
-			},
-			getY: function(incr){
-				return (y1 + y2) + (incr * y2);
-			}
+		origin : {x: (x1 + x2), y: (y1 + y2)},
+		getX: function(incr){
+			return (x1 + x2) + (incr * x2 * 2);
+		},
+		getY: function(incr){
+			return (y1 + y2) + (incr * y2 * 2);
 		}
+	}
 }
+
+var checkCollisions;
+function updateImpact(){
+	checkCollisions = setInterval(function(){
+		updateMonsterPosition();
+		updateBulletPosition();
+		checkCollision();
+	}, 10);
+}
+
+var monstersPosition = Array();
+function updateMonsterPosition(){
+	$(".monster").each(function(){
+		var p = $(this).offset();
+		monsters[$(this)[0].id].x1 = p.left;
+		monsters[$(this)[0].id].x2 = p.left + $(this).width();
+		monsters[$(this)[0].id].y1 = p.top;
+		monsters[$(this)[0].id].y2 = p.top + $(this).height();
+	});
+}
+
+var bulletsPosition = Array();
+function updateBulletPosition(){
+	$(".bullet").each(function(){
+		var p = $(this).offset();
+		bullets[$(this)[0].id].x = p.left;
+		bullets[$(this)[0].id].y = p.top;
+	});
+}
+
+function checkCollision(){
+	for (mKey in monsters) {
+        for (bKey in bullets) {
+        	//console.log("");
+			//console.log(bullets[bKey]);
+			//console.log(monsters[mKey]);
+			//console.log("");
+	        if(bullets[bKey].x >= monsters[mKey].x1
+				&& bullets[bKey].x <= monsters[mKey].x2
+				&& bullets[bKey].y >= monsters[mKey].y1
+				&& bullets[bKey].y <= monsters[mKey].y2){
+	        	lowerHealth(monsters[mKey], bullets[bKey].damage);
+				destroyBullet(bullets[bKey]);
+	        }
+	    }
+    }
+}
+
+function destroyBullet(bullet){
+	bullet.element.remove();
+	clearInterval(bullet.interval);
+	delete bullets[bullet.id];
+	
+}
+
+function lowerHealth(monster, damage){
+	monster.health -= damage;
+	if(monster.health <= 0){
+		destroyMonster(monster);
+	}
+}
+
+function destroyMonster(monster){
+	monster.element.remove();
+	clearInterval(monster.interval);
+	delete monsters[monster.id];	
+}
+
+Object.size = function(obj) {
+    var size = 0, key;
+    for (key in obj) {
+        if (obj.hasOwnProperty(key)) size++;
+    }
+    return size;
+};
